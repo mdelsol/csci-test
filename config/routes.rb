149514be27a1c91ea523880f15f2c8fe67354a96
Matt Smid <msmid@email.uscupstate.edu>
2017-03-31 00:09:16 +0000

    updates 3/30/17

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/delete'
+
   root 'application#goodbye'
+  
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
